ed86b6cb73a6ca6ced5e2e73d7c26e1015baa8c5
Ponggun <wittawat@tt-ss.net>
2025-06-22 01:31:12 +0700

    Consolidate IDocumentRepository interface into Document class and remove separate file

--- src/1.Domain/Entities/Document.cs
+++ src/1.Domain/Entities/Document.cs
@@ -8,4 +8,14 @@ namespace PocLineAPI.Domain
         public Guid Id { get; set; }
         public required string Content { get; set; }
     }
+
+    public interface IDocumentRepository
+    {
+        Task<Document?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Document>> GetAllAsync();
+        Task AddAsync(Document document);
+        Task UpdateAsync(Document document);
+        Task DeleteAsync(Guid id);
+    }
+
 }
