2c5eb5feaedbf009f0a344a46f1445838c443e01
wrm-ponggun <88302281+wrm-ponggun@users.noreply.github.com>
2025-06-27 09:09:03 +0700

    Refactor WebUI: Remove Document management features and update routing to Home controller

--- src/4.Presentation/6.WebUI/Program.cs
+++ src/4.Presentation/6.WebUI/Program.cs
@@ -30,7 +30,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Document}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
